4b28fb0fa93e6ec9fc3ab250de44680e7df93bcc
Mars Borleis <marsborleis@gmail.com>
2026-05-19 22:47:59 -0400

    cachelogic

---
+++ internal/pokecache/cacheLogic.go
@@ -0,0 +1,64 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cacheMap map[string]cacheEntry
+	mu       *sync.Mutex
+}
+
+func (C Cache) NewCache(interval time.Duration) Cache {
+	newCacheMap := Cache{
+		cacheMap: map[string]cacheEntry{},
+		mu:       &sync.Mutex{},
+	}
+
+	go C.reapLoop(interval)
+	return newCacheMap
+}
+
+func (C Cache) Add(key string, val []byte) {
+	C.mu.Lock()
+	defer C.mu.Unlock()
+
+	entry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	C.cacheMap[key] = entry
+}
+
+func (C Cache) Get(key string) ([]byte, bool) {
+	C.mu.Lock()
+	defer C.mu.Unlock()
+
+	entry, exists := C.cacheMap[key]
+	if exists {
+		return entry.val, true
+	} else {
+		return entry.val, false
+	}
+}
+
+func (C Cache) reapLoop(interval time.Duration) {
+	C.mu.Lock()
+	defer C.mu.Unlock()
+
+	tick := time.NewTicker(interval)
+	for range tick.C {
+		for key := range C.cacheMap {
+			targetTime := time.Now().Add(interval * -1)
+			if targetTime.Before(C.cacheMap[key].createdAt) {
+				delete(C.cacheMap, key)
+			}
+		}
+	}
+}
